a68b007a1991981c1abb83d41bd7848879f33352
Wing <wyy3@students.waikato.ac.nz>
2018-07-22 20:21:39 +1200

    Database created, data insertion completed

---
+++ app/src/main/java/com/laika/laika_yellow_book/DbHelper.java
@@ -0,0 +1,87 @@
+package com.laika.laika_yellow_book;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DbHelper extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "Calving.db";
+    public static final String TABLE_NAME = "Calving_Table";
+
+    public static final String COL1 = "CowNum";
+    public static final String COL2 = "DueCalveDate";
+    public static final String COL3 = "SireOfCalf";
+    public static final String COL4 = "CalfBW";
+    public static final String COL5 = "CalvingDate";
+    public static final String COL6 = "CalvingDiff";
+    public static final String COL7 = "Condition";
+    public static final String COL8 = "Sex";
+    public static final String COL9 = "Fate";
+    public static final String COL10 = "CalfIndentNo";
+    public static final String COL11 = "Remarks";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, CowNum INTEGER, DueCalveDate DATE, SireOfCalf INTEGER, CalfBW DOUBLE, CalvingDate DATE, CalvingDiff TEXT, Condition TEXT, Sex TEXT, Fate TEXT, CalfIndentNo INTEGER, Remarks TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(int CowNum, Date DueCalveDate, int SireOfCalf, double CalfBW, Date CalvingDate, String CalvingDiff, String Condition, String Sex, String Fate, int CalfIndentNo, String Remarks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        ContentValues contentValues = new ContentValues(11);
+        contentValues.put(COL1,CowNum);
+        contentValues.put(COL2, dateFormat.format(DueCalveDate));
+        contentValues.put(COL3, SireOfCalf);
+        contentValues.put(COL4, CalfBW);
+        contentValues.put(COL5, dateFormat.format(CalvingDate));
+        contentValues.put(COL6, CalvingDiff);
+        contentValues.put(COL7, Condition);
+        contentValues.put(COL8, Sex);
+        contentValues.put(COL9, Fate);
+        contentValues.put(COL10, CalfIndentNo);
+        contentValues.put(COL11, Remarks);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean updateData(String id, int CowNum, Date DueCalveDate, int SireOfCalf, double CalfBW, Date CalvingDate, String CalvingDiff, String Condition, String Sex, String Fate, int CalfIndentNo, String Remarks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        ContentValues contentValues = new ContentValues(11);
+        contentValues.put(COL1,CowNum);
+        contentValues.put(COL2, dateFormat.format(DueCalveDate));
+        contentValues.put(COL3, SireOfCalf);
+        contentValues.put(COL4, CalfBW);
+        contentValues.put(COL5, dateFormat.format(CalvingDate));
+        contentValues.put(COL6, CalvingDiff);
+        contentValues.put(COL7, Condition);
+        contentValues.put(COL8, Sex);
+        contentValues.put(COL9, Fate);
+        contentValues.put(COL10, CalfIndentNo);
+        contentValues.put(COL11, Remarks);
+        long result = db.update(TABLE_NAME,contentValues,"ID = ?",new String[] {id});
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+}
--- app/src/main/java/com/laika/laika_yellow_book/NewEntryActivity.java
+++ app/src/main/java/com/laika/laika_yellow_book/NewEntryActivity.java
@@ -2,12 +2,52 @@ package com.laika.laika_yellow_book;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class NewEntryActivity extends AppCompatActivity {
-
+    DbHelper myDb;
+    EditText edit_CowNumber, edit_DueCalveDate, edit_SireIfCalf, edit_CalfBW, edit_CalvingDate, edit_CalvingDiff, edit_Condition, edit_Sex, edit_Fate, edit_CalfIndent,edit_Remarks;
+    Button btn_Save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+        myDb = new DbHelper(this);
+
+        edit_CowNumber = (EditText) findViewById(R.id.edit_CowNumber);
+        edit_DueCalveDate = (EditText) findViewById(R.id.edit_DueCalveDate);
+        edit_SireIfCalf = (EditText) findViewById(R.id.edit_SireIfCalf);
+        edit_CalfBW = (EditText) findViewById(R.id.edit_CalfBW);
+        edit_CalvingDate = (EditText) findViewById(R.id.edit_CalvingDate);
+        edit_CalvingDiff = (EditText) findViewById(R.id.edit_CalvingDiff);
+        edit_Condition = (EditText) findViewById(R.id.edit_Condition);
+        edit_Sex = (EditText) findViewById(R.id.edit_Sex);
+        edit_Fate = (EditText) findViewById(R.id.edit_Fate);
+        edit_CalfIndent = (EditText) findViewById(R.id.edit_CalfIndent);
+        edit_Remarks = (EditText) findViewById(R.id.edit_Remarks);
+        btn_Save = (Button) findViewById(R.id.btn_Save);
+    }
+
+    public void AddData(View view) throws ParseException {
+        int cn = Integer.parseInt(edit_CowNumber.getText().toString());
+        String d =  edit_DueCalveDate.getText().toString();
+        Date dcd = new SimpleDateFormat("dd/MM/yyyy").parse(d);
+        String c = edit_CalvingDate.getText().toString();
+        Date cd = new SimpleDateFormat("dd/MM/yyyy").parse(c);
+        int soc = Integer.parseInt(edit_SireIfCalf.getText().toString());
+        double cbw = Double.parseDouble(edit_CalfBW.getText().toString());
+        int cin = Integer.parseInt(edit_CalfIndent.getText().toString());
+        boolean isSuccessful = myDb.insertData(cn,dcd,soc,cbw,cd,edit_CalvingDiff.getText().toString(),edit_Condition.getText().toString(),edit_Sex.getText().toString(),edit_Fate.getText().toString(),cin,edit_Remarks.getText().toString());
+        if(isSuccessful)
+           Toast.makeText(NewEntryActivity.this,"Data is inserted",Toast.LENGTH_LONG).show();
+        else
+           Toast.makeText(NewEntryActivity.this,"Insertion failed",Toast.LENGTH_LONG).show();
     }
 }
