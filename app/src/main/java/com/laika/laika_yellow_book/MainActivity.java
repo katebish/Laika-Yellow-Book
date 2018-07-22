7084649d3a92041e68aab3e957c0ed2885db3ad3
Wing <wyy3@students.waikato.ac.nz>
2018-07-22 14:04:49 +1200

    Initial Project

---
+++ app/src/main/java/com/laika/laika_yellow_book/MainActivity.java
@@ -0,0 +1,20 @@
+package com.laika.laika_yellow_book;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void NewEntryPage(View view) {
+        Intent intent = new Intent(this, NewEntryActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/laika/laika_yellow_book/NewEntryActivity.java
@@ -0,0 +1,13 @@
+package com.laika.laika_yellow_book;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry);
+    }
+}
