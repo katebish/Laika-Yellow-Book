8dbed1963b1a4e3f371cb3e5a483e7e664b8eda9
Kate <katebishop3397@gmail.com>
2018-09-11 12:04:02 +1200

    #3 results returned from db

--- app/src/main/java/com/laika/laika_yellow_book/SearchResults.java
+++ app/src/main/java/com/laika/laika_yellow_book/SearchResults.java
@@ -1,8 +1,10 @@
 package com.laika.laika_yellow_book;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchResults extends AppCompatActivity {
     private DbHelper myDb;
@@ -12,16 +14,20 @@ public class SearchResults extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
         myDb = new DbHelper(this);
-
+        TextView resultsDisplay = null;
+        String text = "";
         if (getIntent().hasExtra("com.laika.laika_yellow_book.SearchContent")){
-            //TextView resultsDisplay = (TextView) findViewById(R.id.searchResBox);
-            EditText searchBox = (EditText) findViewById(R.id.editTextSearch2);
-            String text = getIntent().getExtras().getString("com.laika.laika_yellow_book.SearchContent");
+            resultsDisplay = (TextView) findViewById(R.id.searchResBox);
+            EditText searchBox = findViewById(R.id.editTextSearch2);
+            text = getIntent().getExtras().getString("com.laika.laika_yellow_book.SearchContent");
             searchBox.setText(text);
         }
 
-
-
+        Cursor result = myDb.getDataByID(text);
+        while(result.moveToNext()) {
+            int ID = result.getColumnIndex("ID");
+            resultsDisplay.setText(ID + " ");
+        }
     }
 
 }
